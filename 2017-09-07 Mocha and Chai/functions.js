fb873afe67897afbcc21f066e823e066ea3f122a
Justin H <io@justinh.org>
2017-10-10 15:33:07 -0700

    linting

--- 2017-09-07 Mocha and Chai/functions.js
+++ 2017-09-07 Mocha and Chai/functions.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-const OUTPUT_FILE = process.env.OUTPUT_FILE || 'output.txt';
+const OUTPUT_FILE = process.env.OUTPUT_FILE || 'output.txt'
 
 const lastThreeChars = (str) => {
   const newStr = str.toString()
-  return newStr.slice(-3);
+  return newStr.slice(-3)
 }
 
 const writeToFileXTimes = (str, n) => {
@@ -12,9 +12,8 @@ const writeToFileXTimes = (str, n) => {
   for (let i = 1; i <= n; i++) {
     result += str
   }
-  fs.appendFileSync(OUTPUT_FILE, result);
+  fs.appendFileSync(OUTPUT_FILE, result)
 }
-
 
 module.exports = {
   lastThreeChars,
